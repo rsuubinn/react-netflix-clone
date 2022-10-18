920f78421ad022215c8f3d48f3cebe0c7d759d85
rsuubinn <rsuubinn@gmail.com>
2022-10-18 18:30:15 +0900

    #8.8 Slider part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -7,6 +7,7 @@ import { makeImagePath } from "../utils";
 
 const Wrapper = styled.div`
   background-color: black;
+  padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -41,41 +42,54 @@ const Slider = styled.div`
   position: relative;
 `;
 
-const Row = styled(motion.div)<{ arrayLength: number }>`
+const Row = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(${(props) => props.arrayLength}, 1fr);
-  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
-  background-color: white;
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   height: 200px;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
 `;
 
 const rowVariants = {
   hidden: {
-    x: window.innerWidth + 10,
+    x: window.innerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.innerWidth - 10,
+    x: -window.innerWidth - 5,
   },
 };
+
+const offset = 6;
+
 function Home() {
-  const [click, setClick] = useState(0);
-  const onClick = () => {
-    setClick((prev) => prev + 1);
-  };
+  const [leaving, setLeaving] = useState(false);
+  const [index, setIndex] = useState(0);
   const { data, isLoading } = useQuery<IGetMoviesResults>(
     ["movies", "nowPlaying"],
     getMovies
   );
-  console.log(data);
-
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.ceil(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
   return (
     <Wrapper>
       {isLoading ? (
@@ -83,26 +97,31 @@ function Home() {
       ) : (
         <>
           <Banner
-            onClick={onClick}
+            onClick={increaseIndex}
             bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
           >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
-                arrayLength={data?.results.length!}
                 variants={rowVariants}
                 initial="hidden"
                 animate="visible"
                 exit="exit"
                 transition={{ type: "tween", duration: 1 }}
-                key={click}
+                key={index}
               >
-                {data?.results.map((i) => (
-                  <Box key={i.id}>{i.id}</Box>
-                ))}
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      bgPhoto={makeImagePath(movie.poster_path, "w500")}
+                      key={movie.id}
+                    />
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
